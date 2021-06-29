48f7420975b470a7265f65a9d949dcba3401fbd5
José Monte <josecmontejr@gmail.com>
2021-06-29 05:21:01 -0300

    master | refactory js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,58 @@
-(function timer() {
+const TIMER = {
+  clock: document.querySelector('.timer__clock'),
+  buttons: document.querySelector('.timer__buttons'),
+  seconds: 0,
+  timer: null,
+
+    init() {
+      this.listener();
+    },
+
+    createDateObject(seconds) {
+      const data = new Date(seconds * 1000);
+      return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+      });
+    },
+
+    incrementTimer() {
+      this.timer = setInterval(() => {
+        this.seconds++;
+        this.clock.innerText = this.createDateObject(this.seconds);
+      }, 1000);
+    },
+
+    start() {
+      clearInterval(this.timer);
+      this.clock.classList.contains('timer__clock--active') && this.clock.classList.remove('timer__clock--active');
+      this.incrementTimer(this.seconds);
+    },
+
+    pause() {
+      clearInterval(this.timer);
+      this.clock.classList.add('timer__clock--active');
+    },
+
+    reset() {
+      clearInterval(this.timer);
+      this.seconds = 0;
+      this.clock.classList.contains('timer__clock--active') && this.clock.classList.remove('timer__clock--active');
+      this.clock.innerText = '00:00:00';
+    },
+
+    listener() {
+      this.buttons.addEventListener('click', event => {
+        const element = event.target;
+        if(element.classList.contains('start')) { this.start(); }
+        if(element.classList.contains('pause')) { this.pause(); }
+        if(element.classList.contains('reset')) { this.reset(); }
+      });
+    }
+};
+
+(function main(timer) {
     'use strict';
 
-    const clock = document.querySelector('.timer__clock');
-    const buttons = document.querySelector('.timer__buttons');
-    let seconds = 0;
-    let timer;
-
-    const createDateObject = seconds => {
-        const data = new Date(seconds * 1000);
-        return data.toLocaleTimeString('pt-BR', {
-            hour12: false,
-            timeZone: 'UTC'
-        });
-    }
-
-    const incrementTimer = () => {
-        timer = setInterval(() => {
-            seconds++;
-            clock.innerText = createDateObject(seconds);
-        }, 1000);
-    };
-
-    const actions = {
-        start: function() {
-            clearInterval(timer);
-            clock.classList.contains('timer__clock--active') && clock.classList.remove('timer__clock--active');
-            incrementTimer(seconds);
-        },
-        pause: function() {
-            clearInterval(timer);
-            clock.classList.add('timer__clock--active');
-        },
-        reset: function() {
-            clearInterval(timer);
-            seconds = 0;
-            clock.classList.contains('timer__clock--active') && clock.classList.remove('timer__clock--active');
-            clock.innerText = '00:00:00';
-        }
-    };
-
-    buttons.addEventListener('click', function(event) {
-        const element = event.target;
-        if(element.classList.contains('start')) { actions['start'](); }
-        if(element.classList.contains('pause')) { actions['pause'](); }
-        if(element.classList.contains('reset')) { actions['reset'](); }
-    });
-
-})();
+    timer.init();
+})(TIMER);
